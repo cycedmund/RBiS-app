bdca457368256c6b8e9dcb2d0491dbc4893762ae
cycedmund <cycedmund@gmail.com>
2023-11-01 11:56:09 +0800

    fix: fix isAuthorised

--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const usersCtrl = require("../controllers/usersCtrl");
-const { verifyTrainee } = require("../config/isAuthorised");
+const { verifyTrainee, isAuthorised } = require("../config/isAuthorised");
 
 router.post("/signup", usersCtrl.createUser);
 router.post("/login", usersCtrl.loginUser);
-router.post("/:traineeID/update", verifyTrainee, usersCtrl.updateTraineeStatus);
+router.post(
+  "/:traineeID/update",
+  isAuthorised,
+  verifyTrainee,
+  usersCtrl.updateTraineeStatus
+);
 
 module.exports = router;
